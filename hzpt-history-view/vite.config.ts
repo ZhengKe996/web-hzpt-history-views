003d9847aa08ab78dc2451995f53ccc290f4d59b
zhengke <zhengke996@163.com>
2023-06-04 17:39:16 +0800

    打包配置

--- hzpt-history-view/vite.config.ts
+++ hzpt-history-view/vite.config.ts
@@ -5,6 +5,10 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === 'production'
+      ? 'http://127.0.0.1:7001/public/views/'
+      : '/',
   plugins: [
     vue(),
     createSvgIconsPlugin({
